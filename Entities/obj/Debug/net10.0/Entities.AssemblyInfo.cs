be134fb61a74246720c3e79f85edcceb3c84b7b0
Piotr Junosz <piotrju2005@wp.pl>
2025-09-08 17:29:59 +0200

    create interfaces

--- Entities/obj/Debug/net10.0/Entities.AssemblyInfo.cs
+++ Entities/obj/Debug/net10.0/Entities.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Entities")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+53719ecf6c098b0788eaf0111559b8d701a4a1a9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e0123c742f7507618aae670a36a5b4081a2f44e7")]
 [assembly: System.Reflection.AssemblyProductAttribute("Entities")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Entities")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- RepositoryContracts/obj/Debug/net10.0/RepositoryContracts.AssemblyInfo.cs
+++ RepositoryContracts/obj/Debug/net10.0/RepositoryContracts.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("RepositoryContracts")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d42b9b073687dd47f13e1d2248d1e927e247ac00")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e0123c742f7507618aae670a36a5b4081a2f44e7")]
 [assembly: System.Reflection.AssemblyProductAttribute("RepositoryContracts")]
 [assembly: System.Reflection.AssemblyTitleAttribute("RepositoryContracts")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

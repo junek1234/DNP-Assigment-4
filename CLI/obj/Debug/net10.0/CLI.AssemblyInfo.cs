b083ee147d5134e18c632943b8805ffc668a8310
Piotr Junosz <piotrju2005@wp.pl>
2025-09-13 21:36:28 +0200

    ?

--- CLI/obj/Debug/net10.0/CLI.AssemblyInfo.cs
+++ CLI/obj/Debug/net10.0/CLI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CLI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2ddca2a4da8c37f955f990cce5efab97577a94d9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+755c915bdaac13e6fdcd55fe36088e43dce62142")]
 [assembly: System.Reflection.AssemblyProductAttribute("CLI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CLI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
